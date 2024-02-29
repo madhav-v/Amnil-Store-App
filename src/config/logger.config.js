f7bae305ce23e1c814184cb577314543794e4d04
madhav-v <madhavdhungana36@gmail.com>
2024-02-29 20:35:21 +0545

    Logs added in file

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,10 +1,30 @@
 const { createLogger, transports, format } = require("winston");
+const fs = require("fs");
+const path = require("path");
+
+const logsDir = "logs";
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+const logFilePath = path.join(logsDir, "app.log");
 
 const logger = createLogger({
   transports: [
+    // Log to console
     new transports.Console({
       format: format.combine(
         format.colorize(),
+        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      ),
+    }),
+    // Log to file
+    new transports.File({
+      filename: logFilePath,
+      format: format.combine(
         format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
         format.printf(
           (info) => `${info.timestamp} ${info.level}: ${info.message}`
